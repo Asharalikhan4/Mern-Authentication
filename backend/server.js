f3c84af6254159e41ea6c12347d8b917bd9d5a97
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-02-21 15:51:13 +0530

    Backend routing completed

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from server side");
+});
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server is up at ${port}`);
+});
