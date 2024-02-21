da2e3a0c7cf1471419b950451c864dbc8663411d
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-02-21 17:34:48 +0530

    logout and update user api added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile } from "../controllers/userController.js";
 const router = express.Router();
+
+import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile } from "../controllers/userController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
